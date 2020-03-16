f8d198b33daca63ef54134f6c19ec6635dcb66a9
Laurent_Vinn <laurent.mundell@gmail.com>
2020-03-16 13:53:40 -0400

    code was written in master but will be py scripted later

--- scripts/methods_globals_constants.py
+++ scripts/methods_globals_constants.py
@@ -9,7 +9,7 @@ def feature_extract_mean_count_median(df, columns, target, return_trig=[True, Tr
     columns     : [List of Strings]  : list of catagory column names
     target      : [String]           : index of where the target column is in the dataframe
     return_trig : [List of Boolean]  : triggers for what extractions to  perform.
-                                       Index 1 = Counts, index 2 = mean, index 3 = median 
+                                       Index 0 = Counts, index 1 = mean, index 2 = median 
     
     Returns
     -------
@@ -27,4 +27,26 @@ def feature_extract_mean_count_median(df, columns, target, return_trig=[True, Tr
             _ =  df.groupby(name).median()[target]
             df[f'{name}_Median'] = df[name].map(lambda value: _[value])
 
-			
+def fillna_average_by_target_column(row, avg_dict, target_col, effected_col):
+    """
+    When given a row of a dataframe, this method will use the target column to fill nan values with 
+    the average associated with the catagorical values in the effected column. 
+    
+    Parameters
+    ----------
+    row 			: [Pandas Series] : A row of a dataframe.
+    avg_dict 		: [Dictionary] 	  : A dictionary of the average of the catagorical values in the target column of the dataframe
+    target_col 		: [int] 		  : index of where the catagorical target is to base means on
+    effected_col 	: [int] 		  : index of where the effected column is in the dataframe
+    
+    Returns
+    -------
+    a dataframe row with effect column value changed if it is null
+    """
+    try:
+        if np.isnan(row[effected_col]):      
+            row[effected_col] = np.round(avg_dict[row[target_col]], 2)
+    except:
+        pass #row[effected_col] = "???"
+        
+    return row
